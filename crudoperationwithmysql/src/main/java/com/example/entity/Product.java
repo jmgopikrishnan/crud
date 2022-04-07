c6ee8a0f6b6ce0e78b5121c7418c8666ee139656
jmgopikrishnan <78915443+jmgopikrishnan@users.noreply.github.com>
2022-04-07 19:34:40 +0800

    Add files via upload

---
+++ crudoperationwithmysql/src/main/java/com/example/entity/Product.java
@@ -0,0 +1,51 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="product_table")
+public class Product {
+	
+@Id
+@GeneratedValue
+private int id;
+private String name;
+private int quantity;
+
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getQuantity() {
+	return quantity;
+}
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+private double price;
+}
